4844851fc080fe1d6df13a1169b465492fdc5fdf
Dana Holubová <danmich@email.cz>
2023-11-06 10:12:31 +0100

    Upraveny intervaly, překlep a výsledný výpis.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
 //vstup by mel byt v budoucnu osetreny, aby bylo mozne zadat jen cele kladne cislo a horni limit (zasoby tricek budou omezene)
 let pocetKusu = Number(prompt("Zadejte počet kusů: "))
-let celkovaCena = 0
+//let celkovaCena = 0
+let celkovaCena //u let není potřeba inicializovat, stačí deklarovat
 
 
 if (pocetKusu >= 0 && pocetKusu < 50) {
     celkovaCena = 300 * pocetKusu
 }
 
-else if (pocetKusu >= 50 && pocetKusu < 200) {
+else if (pocetKusu >= 50 && pocetKusu <= 200) {
     celkovaCena = 250 * pocetKusu
 }
 
-else if (pocetKusu >= 200 && pocetKusu < 500) {
+else if (pocetKusu > 200 && pocetKusu <= 500) {
     celkovaCena = 200 * pocetKusu
 }
 
-else if (pocetKusu >= 500 && pocetKusu < 1000) {
+else if (pocetKusu > 500 && pocetKusu <= 1000) {
     celkovaCena = 150 * pocetKusu
 }
 
-else if (pocetKusu >= 1000) {
+else if (pocetKusu > 1000) {
     celkovaCena = 120 * pocetKusu
 }
 
@@ -28,7 +29,12 @@ else { celkovaCena = 'nelze spočítat' }
 
 console.log('Celková cena: ' + celkovaCena)
 
-document.body.innerHTML += `Ceková cena je: ${celkovaCena} Kč`
+document.body.innerHTML += `
+<p>Počet kusů: ${pocetKusu}</p>
+<p>Cena za kus: ${celkovaCena / pocetKusu} Kč</p>
+<p>Celková cena je: ${celkovaCena} Kč</p>
+`
+
 
 
 
